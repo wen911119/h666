13d68b8ee0d604847497f7855ec91b210ec2d3ff
wen911119 <wen911119@gmail.com>
2019-08-30 19:15:45 +0800

    cdn test

--- demo/src/pages/index/app.js
+++ demo/src/pages/index/app.js
@@ -50,7 +50,7 @@ export default class Index extends Component {
   render () {
     return (
       <div>
-        Index-6
+        Index-7
         <div className='test'>{this.state.name}</div>
         <div onClick={this.goto}>go to buttonDemo</div>
         <div onClick={this.goto2}>go to actionsheetDemo</div>
