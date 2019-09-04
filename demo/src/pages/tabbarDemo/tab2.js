f52b95a171cd893744184603ffcb96e32076030e
wen911119 <wen911119@gmail.com>
2019-09-04 18:27:59 +0800

    update

--- demo/src/pages/tabbarDemo/tab2.js
+++ demo/src/pages/tabbarDemo/tab2.js
@@ -6,15 +6,19 @@ export default class Item2 extends Component {
     super(props)
     console.log('tab-item-2===>constructor')
   }
+
   onActive = () => {
     console.log('tab-item-2 is active!')
   }
+
   componentDidMount () {
     console.log('tab-item-2', 'componentDidMount')
   }
+
   componentWillUnmount () {
     console.log('tab-item-2', 'componentWillUnmount')
   }
+
   render () {
     return <Text>tab-item-2</Text>
   }
