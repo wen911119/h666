1ac1b933b4556afb47ece9f993098fd18b2f181c
wen911119 <wen911119@gmail.com>
2019-09-20 15:39:49 +0800

    update:完善demo

--- demo/src/pages/textDemo/app.js
+++ demo/src/pages/textDemo/app.js
@@ -1,17 +1,26 @@
-import { h, Component } from 'preact'
+import { h } from 'preact'
+import Text from '@ruiyun/preact-text'
+import { SlotColumnView } from '@ruiyun/preact-layout-suite'
 
 import DemoPage from '../../components/DemoPage'
-import style from './app.css'
 
-export default class TextDemo extends Component {
-  state = {
-    name: 'wenjun3'
-  }
-  render () {
-    return (
-      <DemoPage title='Text'>
-        <div className={style.test}>{this.state.name}</div>
-      </DemoPage>
-    )
-  }
-}
+const TextDemo = () => (
+  <DemoPage title='Text'>
+    <SlotColumnView
+      hAlign='center'
+      vAlign='center'
+      margin={[0, 30, 0, 30]}
+      padding={[50, 0, 50, 0]}
+      bgColor='#fff'
+      slot={10}
+    >
+      <Text>这是一句默认文本</Text>
+      <Text size={40}>这是一句大一点的文本</Text>
+      <Text color='#ccc'>这是一句灰一点的文本</Text>
+      <Text weight='bold'>这是一句粗一点的文本</Text>
+      <Text weight='bold' size={24} color='#f8584f'>这是一句完全不同的文本</Text>
+    </SlotColumnView>
+  </DemoPage>
+)
+
+export default TextDemo
