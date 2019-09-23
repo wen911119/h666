f085f4543803e91c13eb33771a637d3f4162cc55
wen911119 <wen911119@gmail.com>
2019-09-23 18:50:55 +0800

    update:switch-demo

--- demo/src/pages/switchDemo/app.js
+++ demo/src/pages/switchDemo/app.js
@@ -1,53 +1,37 @@
-import { h, Component } from 'preact'
+import { h } from 'preact'
 import Switch from '@ruiyun/preact-switch'
+import { SlotColumnView, SlotRowView } from '@ruiyun/preact-layout-suite'
+import Text from '@ruiyun/preact-text'
 
-export default class SwitchDemo extends Component {
-  state = {
-    switch1: true,
-    switch2: false,
-    switch3: true,
-    switch4: false
-  }
-  onChange1 = v => {
-    console.log('switch1=>', v)
-  }
-  onChange2 = v => {
-    console.log('switch2=>', v)
-  }
-  onReset = () => {
-    this.setState({ switch1: true, switch2: false })
-  }
-  render () {
-    console.log('render SwitchDemo')
-    return (
-      <div>
-        SwitchDemo
-        <div>
-          switch1(默认)
-          <Switch
-            value={this.state.switch1}
-            onChange={this.onChange1}
-          />
-        </div>
-        <div>
-          switch2(自定义大小颜色)
-          <Switch
-            value={this.state.switch2}
-            color='#fc9153'
-            size={150}
-            onChange={this.onChange2}
-          />
-        </div>
-        <div>
-          switch3(默认+on+disabled)
-          <Switch disabled value={this.state.switch3} />
-        </div>
-        <div>
-          switch4(默认+off+disabled)
-          <Switch disabled value={this.state.switch4} />
-        </div>
-        <div onClick={this.onReset}>重置</div>
-      </div>
-    )
-  }
-}
+import DemoPage from '../../components/DemoPage'
+
+const SwitchDemo = () => (
+  <DemoPage title='Switch'>
+    <SlotColumnView
+      bgColor='#fff'
+      margin={[30, 30, 30, 30]}
+      hAlign='center'
+      padding={[50, 0, 50, 0]}
+      slot={30}
+    >
+      <SlotRowView slot={30}>
+        <Text>默认</Text>
+        <Switch />
+      </SlotRowView>
+      <SlotRowView slot={30}>
+        <Text>自定义大小</Text>
+        <Switch size={150} />
+      </SlotRowView>
+      <SlotRowView slot={30}>
+        <Text>自定义颜色</Text>
+        <Switch color='#fc9153' value />
+      </SlotRowView>
+      <SlotRowView slot={30}>
+        <Text>自定义颜色+disabled</Text>
+        <Switch color='#fc9153' value disabled />
+      </SlotRowView>
+    </SlotColumnView>
+  </DemoPage>
+)
+
+export default SwitchDemo
