6810ac943cc8c570e3f066179582402355afada2
wen911119 <wen911119@gmail.com>
2019-09-23 12:00:10 +0800

    update:image-demo

--- demo/src/pages/imageDemo/app.js
+++ demo/src/pages/imageDemo/app.js
@@ -1,16 +1,54 @@
-import { h, Component } from 'preact'
-import style from './app.css'
+import { h } from 'preact'
+import Text from '@ruiyun/preact-text'
+import { SlotColumnView } from '@ruiyun/preact-layout-suite'
+import Image from '@ruiyun/preact-image'
 
-export default class ImageDemo extends Component {
-  state = {
-    name: 'wenjun'
-  }
-  render () {
-    return (
-      <div>
-        ImageDemo
-        <div className={style.test}>{this.state.name}</div>
-      </div>
-    )
-  }
-}
+import DemoPage from '../../components/DemoPage'
+import demoImage from '../../assets/img-demo.jpg'
+
+const ImageDemo = () => (
+  <DemoPage title='Image'>
+    <SlotColumnView slot={30} hAlign='center' padding={[30, 30, 30, 30]}>
+      <Text size={24} color='#919191'>
+        默认模式，严格按照所设置宽高显示（原图700*375，显示400*200）
+      </Text>
+      <Image
+        src={demoImage}
+        width={400}
+        height={200}
+        style={{ border: '1px solid #f8584f', boxSizing: 'content-box' }}
+      />
+      <Text size={24} color='#919191'>
+        默认模式设置的宽高比例和原图比例不一致则会有拉伸（原图700*375，显示400*400）
+      </Text>
+      <Image
+        src={demoImage}
+        width={400}
+        height={400}
+        style={{ border: '1px solid #f8584f', boxSizing: 'content-box' }}
+      />
+      <Text size={24} color='#919191'>
+        fit模式，保证不裁剪不拉伸但有留白（原图700*375，显示400*400）
+      </Text>
+      <Image
+        src={demoImage}
+        width={400}
+        height={400}
+        mode='fit'
+        style={{ border: '1px solid #f8584f', boxSizing: 'content-box' }}
+      />
+      <Text size={24} color='#919191'>
+        fill模式，保证不留白不拉伸但有裁剪（原图700*375，显示400*400）
+      </Text>
+      <Image
+        src={demoImage}
+        width={400}
+        height={400}
+        mode='fill'
+        style={{ border: '1px solid #f8584f', boxSizing: 'content-box' }}
+      />
+    </SlotColumnView>
+  </DemoPage>
+)
+
+export default ImageDemo
