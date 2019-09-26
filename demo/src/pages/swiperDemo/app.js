7e0b3c938680eea73880a37f93bbb793389e2b13
wen911119 <wen911119@gmail.com>
2019-09-26 16:25:42 +0800

    update:完善swiper-demo

--- demo/src/pages/swiperDemo/app.js
+++ demo/src/pages/swiperDemo/app.js
@@ -1,108 +1,76 @@
 import { h, Component } from 'preact'
-import { XCenterView, ColumnView, RowView } from '@ruiyun/preact-layout-suite'
+import { XCenterView, RowView } from '@ruiyun/preact-layout-suite'
 import Swiper from '@ruiyun/preact-m-swiper'
 import Button from '@ruiyun/preact-button'
-import AutoList from '@ruiyun/preact-m-auto-list'
 import Text from '@ruiyun/preact-text'
-import Line from '@ruiyun/preact-line'
-import Ajax from '@ruiyun/ajax'
+
+import DemoPage from '../../components/DemoPage'
+
+const Item1 = props => {
+  console.log('render-item-1', props.active)
+  return (
+    <RowView
+      height={414}
+      hAlign='center'
+      bgColor='#66CC00'
+      margin={[0, 30, 0, 30]}
+    >
+      <Text color='#fff'>{props.text}</Text>
+    </RowView>
+  )
+}
+
+const Item2 = props => {
+  console.log('render-item-2', props.active)
+  return (
+    <RowView
+      height={414}
+      hAlign='center'
+      bgColor='#99CCFF'
+      margin={[0, 30, 0, 30]}
+    >
+      <Text color='#fff'>{props.text}</Text>
+    </RowView>
+  )
+}
 
 const Item3 = props => {
   console.log('render-item-3', props.active)
   return (
-    <XCenterView height={414}>
-      <Button color='#f8584f' onClick={props.onClick} height={80} width={240}>
-        回到第二张
+    <RowView
+      height={414}
+      hAlign='center'
+      bgColor='#fff'
+      margin={[0, 30, 0, 30]}
+    >
+      <Button color='#f8584f' onClick={props.onClick} height={80} width={350}>
+        点我回到第二张
       </Button>
-    </XCenterView>
-  )
-}
-const Item1 = props => {
-  console.log('render-item-1', props.active)
-  return (
-    <XCenterView height={414}>
-      <Button color='#f8584f' height={80} width={240}>
-        {props.text}
-      </Button>
-    </XCenterView>
+    </RowView>
   )
 }
 export default class ScrollerDemo extends Component {
   state = {
-    current: 0,
-    text: '第一张',
-    params: {
-      keyword: ''
-    }
+    current: 0
   }
   onSwipe = index => {
-    console.log('swiped===>', index)
     this.setState({ current: index })
   }
   doSwipe = () => {
     this.setState({ current: 1, text: '第一张2' })
   }
-  fetchListData = async params => {
-    const ret = await Ajax.get(
-      'https://uapi.dev.quancheng-ec.com/rhea/hospitals',
-      {
-        params,
-        headers: {
-          loading: 'false',
-          identifier: 'hrg-mp'
-        }
-      }
-    )
-    if (ret && ret.success) {
-      return ret.result
-    }
-    return {
-      data: [],
-      pageInfo: {
-        currentPage: params.pageNum
-      }
-    }
-  }
-  keyExtractor = item => item.hospitalId
-  format = ret => {
-    let res = {
-      list: ret.data,
-      pageInfo: {
-        totalPage: Math.ceil(ret.pageInfo.total / ret.pageInfo.pageSize),
-        currentPage: ret.pageInfo.currentPage
-      }
-    }
-    return res
-  }
-  renderItem = item => (
-    <div>
-      <ColumnView padding={[0, 30, 0, 30]} bgColor='#fff'>
-        <RowView height={88}>
-          <Text color='#535353' size={28}>
-            {item.name}
-          </Text>
-        </RowView>
-        <Line />
-      </ColumnView>
-    </div>
-  )
   render () {
     return (
-      <Swiper onChange={this.onSwipe} activeIndex={this.state.current}>
-        <Item1 text={this.state.text} />
-        <AutoList
-          pageSize={20}
-          alias={{ pageNum: 'page', pageSize: 'page_size' }}
-          params={this.state.params}
-          fetchListData={this.fetchListData}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-          format={this.format}
-          ref={s => (this.list = s)}
-          height='400px'
-        />
-        <Item3 onClick={this.doSwipe} />
-      </Swiper>
+      <DemoPage title='Swiper'>
+        <Swiper onChange={this.onSwipe} activeIndex={this.state.current}>
+          <Item1 text='左滑切换到第二个' />
+          <Item2 text='这是第二个' />
+          <Item3 onClick={this.doSwipe} />
+        </Swiper>
+        <XCenterView height={100}>
+          <Text>{`(${this.state.current + 1}/3)`}</Text>
+        </XCenterView>
+      </DemoPage>
     )
   }
 }
