827bef00c616b8bf9224ff5f107ab6004ced3649
wen911119 <wen911119@gmail.com>
2019-10-14 15:27:57 +0800

    update:更新demo

--- demo/src/pages/tabsDemo/app.js
+++ demo/src/pages/tabsDemo/app.js
@@ -1,26 +1,59 @@
-import { h, Component } from 'preact'
-import { XCenterView } from '@ruiyun/preact-layout-suite'
+import { h } from 'preact'
+import {
+  XCenterView,
+  RowView,
+  SlotColumnView
+} from '@ruiyun/preact-layout-suite'
 import Text from '@ruiyun/preact-text'
 import Tabs from '@ruiyun/preact-m-tabs'
+import { useState, useCallback } from 'preact/hooks'
+import Switch from '@ruiyun/preact-switch'
 
-const SwiperItem = () => (
+import DemoPage from '../../components/DemoPage'
+
+const ShadowUpdater = ({ onChange, value }) => (
+  <RowView>
+    <Text color='#919191' size={26}>
+      shadow : &nbsp;
+    </Text>
+    <Switch size={80} value={value} onChange={onChange} />
+  </RowView>
+)
+
+const TabItem = ({ children }) => (
   <XCenterView height={600}>
-    <Text>hello</Text>
+    <Text color='#FFCCCC'>{children}</Text>
   </XCenterView>
 )
 
-export default class TabsDemo extends Component {
-  render () {
-    return (
-      <Tabs titles={['tab1', 'tab2', 'tab3']}>
-        <SwiperItem />
-        <XCenterView height={600}>
-          <div>jun</div>
-        </XCenterView>
-        <XCenterView height={600}>
-          <div>jun2</div>
-        </XCenterView>
+const TabsDemo = () => {
+  const [tabsConfig, updateConfig] = useState({
+    titles: ['全部', '待付款', '处理中'],
+    titleSize: 28,
+    titleColor: '#919191',
+    activeTitleColor: '#f8584f',
+    indicatorColor: '#f8584f',
+    headerHeight: 100,
+    shadow: true
+  })
+  const updateConfigOf = attribute => v =>
+    updateConfig(oldConfig => Object.assign({}, oldConfig, { [attribute]: v }))
+  return (
+    <DemoPage title='Tabs'>
+      <SlotColumnView slot={20} padding={[30, 30, 30, 30]}>
+        <ShadowUpdater
+          value={tabsConfig.shadow}
+          onChange={updateConfigOf('shadow')}
+        />
+      </SlotColumnView>
+
+      <Tabs {...tabsConfig}>
+        <TabItem>全部订单(滑动切换)</TabItem>
+        <TabItem>待付款的订单</TabItem>
+        <TabItem>处理中的订单</TabItem>
       </Tabs>
-    )
-  }
+    </DemoPage>
+  )
 }
+
+export default TabsDemo
