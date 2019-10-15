fae0d6dbc0681e8bf650981ad2c04a0b081371fe
wen911119 <wen911119@gmail.com>
2019-10-15 17:58:17 +0800

    feat:picker-demo

--- demo/src/pages/pickerDemo/app.js
+++ demo/src/pages/pickerDemo/app.js
@@ -1,7 +1,33 @@
 import { h, Component } from 'preact'
 import { WithPicker } from '@ruiyun/preact-m-picker'
+import { RowView, SlotColumnView } from '@ruiyun/preact-layout-suite'
+import Text from '@ruiyun/preact-text'
+import Icon from '@ruiyun/preact-icon'
+import DemoPage from '../../components/DemoPage'
 
 const options = ['安徽', '上海', '深圳', '重庆', '北京', '四川', '湖南']
+
+const PickerItem = ({ onClick, values, children }) => (
+  <RowView
+    padding={[30, 30, 30, 30]}
+    onClick={onClick}
+    bgColor='#fff'
+    hAlign='between'
+  >
+    <Text>{children}</Text>
+    <SlotColumnView>
+      {values
+        .map(i => options[i])
+        .map(item => (
+          <Text color='#CC6699' key={item}>
+            {item}
+          </Text>
+        ))}
+    </SlotColumnView>
+
+    <Icon name='icon-qianjin' color='#919191' />
+  </RowView>
+)
 
 @WithPicker
 export default class PickerDemo extends Component {
@@ -40,19 +66,12 @@ export default class PickerDemo extends Component {
   render () {
     const { picker1, picker2 } = this.state
     return (
-      <div>
-        PickerDemo
-        <div onClick={this.openPicker1}>
-          {picker1 && picker1.length
-            ? picker1.map(i => options[i]).join(',')
-            : '请选择籍贯'}
-        </div>
-        <div onClick={this.openPicker2}>
-          {picker2 && picker2.length
-            ? picker2.map(i => options[i]).join(',')
-            : '请选择喜欢的城市（多选）'}
-        </div>
-      </div>
+      <DemoPage title='Picker'>
+        <SlotColumnView padding={[0, 30, 0, 30]} slot={30}>
+          <PickerItem values={picker1} onClick={this.openPicker1}>打开picker(单选)</PickerItem>
+          <PickerItem values={picker2} onClick={this.openPicker2}>打开picker(多选)</PickerItem>
+        </SlotColumnView>
+      </DemoPage>
     )
   }
 }
