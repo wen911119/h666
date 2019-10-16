e18a11883d2c3cfb6aeb1422633ba5b6e5b7a16b
wen911119 <wen911119@gmail.com>
2019-10-16 11:54:39 +0800

    feat:图片预览的demo完成

--- demo/src/pages/imagePreviewDemo/app.js
+++ demo/src/pages/imagePreviewDemo/app.js
@@ -1,16 +1,48 @@
 import { h, Component } from 'preact'
-import style from './app.css'
+import Text from '@ruiyun/preact-text'
+import { RowView } from '@ruiyun/preact-layout-suite'
+import WithImagePreview from '@ruiyun/preact-m-image-preview'
+import Image from '@ruiyun/preact-image'
+import DemoPage from '../../components/DemoPage'
 
+@WithImagePreview
 export default class ImagePreviewDemo extends Component {
   state = {
-    name: 'wenjun'
+    images: [
+      'https://h666-demo.ruiyun2015.com/1910151352-1.jpg',
+      'https://h666-demo.ruiyun2015.com/1910151352-2.jpg',
+      'https://h666-demo.ruiyun2015.com/1910151352-5.jpg'
+    ]
+  }
+  preview = index => {
+    this.props.$preview(this.state.images, index)
   }
   render () {
     return (
-      <div>
-        ImagePreviewDemo
-        <div className={style.test}>{this.state.name}</div>
-      </div>
+      <DemoPage title='ImagePreview'>
+        <RowView hAlign='center'>
+          <Text size={26} color='#CC9999'>
+            点击下面的任意一张图进入预览模式(可滑动切换)
+          </Text>
+        </RowView>
+        <RowView
+          hAlign='between'
+          padding={[30, 15, 30, 15]}
+          bgColor='#fff'
+          margin={[30, 30, 30, 30]}
+        >
+          {this.state.images.map((url, index) => (
+            <Image
+              key={url}
+              src={url}
+              width={200}
+              height={100}
+              // eslint-disable-next-line
+              onClick={this.preview.bind(this, index)}
+            />
+          ))}
+        </RowView>
+      </DemoPage>
     )
   }
 }
