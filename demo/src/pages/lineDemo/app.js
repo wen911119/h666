10ad76ed8a9b43b635116eb2b4806cf8166e6894
wen911119 <wen911119@gmail.com>
2019-10-17 16:38:09 +0800

    feat:线条 demo

--- demo/src/pages/lineDemo/app.js
+++ demo/src/pages/lineDemo/app.js
@@ -1,16 +1,32 @@
-import { h, Component } from 'preact'
-import style from './app.css'
+import { h } from 'preact'
+import { SlotColumnView, RowView } from '@ruiyun/preact-layout-suite'
+import Text from '@ruiyun/preact-text'
+import Line from '@ruiyun/preact-line'
+import DemoPage from '../../components/DemoPage'
 
-export default class LineDemo extends Component {
-  state = {
-    name: 'wenjun'
-  }
-  render () {
-    return (
-      <div>
-        LineDemo
-        <div className={style.test}>{this.state.name}</div>
-      </div>
-    )
-  }
-}
+const LineDemo = () => (
+  <DemoPage title='Line'>
+    <SlotColumnView
+      margin={[30, 30, 30, 30]}
+      padding={[30, 0, 30, 0]}
+      bgColor='#fff'
+      hAlign='center'
+      slot={30}
+    >
+      <Text>默认(水平1像素)：</Text>
+      <Line />
+      <Text>加粗：</Text>
+      <Line size='4px' />
+      <Text>变色：</Text>
+      <Line color='#f8584f' />
+      <Text>两端缩进：</Text>
+      <Line color='#CCCCFF' indent={[30, 30]} />
+      <Text>纵向：</Text>
+      <RowView height={60}>
+        <Line color='#CCCCFF' v />
+      </RowView>
+    </SlotColumnView>
+  </DemoPage>
+)
+
+export default LineDemo
