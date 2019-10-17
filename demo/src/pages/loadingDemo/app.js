f59abbc9a15f76b0a6df1a26890ccff325f09942
wen911119 <wen911119@gmail.com>
2019-10-17 16:11:17 +0800

    feat:loading demo

--- demo/src/pages/loadingDemo/app.js
+++ demo/src/pages/loadingDemo/app.js
@@ -1,5 +1,8 @@
 import { h, Component } from 'preact'
-import style from './app.css'
+import { RowView, SlotColumnView } from '@ruiyun/preact-layout-suite'
+import Text from '@ruiyun/preact-text'
+import Loading from '@ruiyun/preact-loading'
+import DemoPage from '../../components/DemoPage'
 
 export default class LoadingDemo extends Component {
   state = {
@@ -7,10 +10,28 @@ export default class LoadingDemo extends Component {
   }
   render () {
     return (
-      <div>
-        LoadingDemo
-        <div className={style.test}>{this.state.name}</div>
-      </div>
+      <DemoPage title='Loading'>
+        <SlotColumnView
+          slot={30}
+          bgColor='#fff'
+          hAlign='center'
+          margin={[30, 30, 30, 30]}
+          padding={[30, 30, 30, 30]}
+        >
+          <RowView>
+            <Text>默认：</Text>
+            <Loading />
+          </RowView>
+          <RowView>
+            <Text>加大：</Text>
+            <Loading size={60} />
+          </RowView>
+          <RowView>
+            <Text>变色：</Text>
+            <Loading color='#99CC66' />
+          </RowView>
+        </SlotColumnView>
+      </DemoPage>
     )
   }
 }
