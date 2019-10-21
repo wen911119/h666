3c17de954d57d38a5109635cbbe4c5e4e3d3fcc3
wen911119 <wen911119@gmail.com>
2019-10-21 17:25:01 +0800

    feat:云函数的demo

--- demo/wechat/app.js
+++ demo/wechat/app.js
@@ -3,5 +3,18 @@ App({
   globalData: {
     // host: 'http://10.100.1.165:3000'
     host: 'https://h666-demo.ruiyun2015.com'
+  },
+  onLaunch(options) {
+    // Do something initial when launch.
+    console.log(options)
+    wx.cloud.init()
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'openid',
+      success: function (res) {
+        console.log(res.result) // 3
+      },
+      fail: console.error
+    })
   }
 })
