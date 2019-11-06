7e94e5c5a94e16b7c627f4dd9695e7f3a5f2ad79
wen911119 <wen911119@gmail.com>
2019-11-06 17:46:11 +0800

    h5+debug

--- demo/src/pages/timing/app.js
+++ demo/src/pages/timing/app.js
@@ -1,14 +1,27 @@
 import { h, Component } from 'preact'
+import Indicator from 'h5-indicator'
 import style from './app.css'
 
 export default class Timing extends Component {
   state = {
     name: 'wenjun'
   }
+  componentDidMount () {
+    document.addEventListener(
+      'plusready',
+      () => {
+        Indicator.toast('plusready', {
+          timeout: 10000
+        })
+      },
+      false
+    )
+  }
   render () {
     return (
       <div>
         Timing
+        <input type='file' name='audio' accept='video/*' capture='user' />
         <div className={style.test}>{this.state.name}</div>
       </div>
     )
