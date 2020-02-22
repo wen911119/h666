a2d831d5c9bc1856fa74413dedb38ed380f10494
wen911119 <wen911119@gmail.com>
2020-02-22 23:30:50 +0800

    debug:手动更新

--- demo/src/entry.js
+++ demo/src/entry.js
@@ -1,5 +1,4 @@
 import { setIconFontUrl } from '@ruiyun/preact-icon'
-
 export default function onEntry () {
   // 下面的代码每个页面初始化时都会执行
   setIconFontUrl('https://at.alicdn.com/t/font_1401630_2vgk76dau2a.css')
