97363fd7f63d130880f4c920c2fe3cd8e1a75131
wen911119 <wen911119@gmail.com>
2020-02-27 14:44:10 +0800

    cli 修改

--- demo/src/entry.js
+++ demo/src/entry.js
@@ -2,15 +2,5 @@ import { setIconFontUrl } from '@ruiyun/preact-icon'
 
 export default function onEntry() {
   // 下面的代码每个页面初始化时都会执行
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
-  if ('serviceWorker' in navigator && !isIOS) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        console.log('service worker 注册完成', registration);
-      }, function(err) {
-        console.log('注册失败', err);
-      });
-    });
-  }
   setIconFontUrl('https://at.alicdn.com/t/font_1401630_2vgk76dau2a.css')
 }
