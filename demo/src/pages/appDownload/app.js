64bad5e884b94ee70c46d87eb330a4baf9456f39
wen911119 <wen911119@gmail.com>
2020-04-13 17:39:44 +0800

    feat:增加下载页面

---
+++ demo/src/pages/appDownload/app.js
@@ -0,0 +1,16 @@
+import { h, Component } from 'preact'
+
+export default class AppDownload extends Component {
+
+  render() {
+    return (
+      <div>
+        <div>
+          <a href='https://h666-demo.ruiyun2015.com/h666Hybird.apk' download>
+            安卓下载
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
