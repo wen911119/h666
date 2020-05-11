8664f9d40fe0867269c8acec152cfa76081c3d04
wen911119 <wen911119@gmail.com>
2020-05-11 14:10:24 +0800

    feat:修改缓存头

--- demo/upload.js
+++ demo/upload.js
@@ -19,7 +19,7 @@ if (region && accessKeyId && accessKeySecret && bucket) {
     files.forEach(function(file) {
       console.log(basePath + '/' + file)
       const cacheControl = file.includes('.html')
-        ? 'no-cache'
+        ? 'max-age=0, s-maxage=63072000'
         : file.includes('sw.js') || file.includes('app.json')
         ? 'no-store'
         : 'max-age=63072000'
