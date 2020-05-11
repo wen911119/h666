fc9dccf66f08c117b83c8fa6dfdb64dc10c3e904
wen911119 <wen911119@gmail.com>
2020-05-11 23:04:49 +0800

    feat:indicator demo

--- demo/src/pages/indicatorDemo/app.js
+++ demo/src/pages/indicatorDemo/app.js
@@ -1,16 +1,44 @@
 import { h, Component } from 'preact'
-import style from './app.css'
+import Button from '@ruiyun/preact-button'
+import { SlotColumnView } from '@ruiyun/preact-layout-suite'
+import Indicator from 'h5-indicator'
+
+import DemoPage from '../../components/DemoPage'
 
 export default class IndicatorDemo extends Component {
-  state = {
-    name: 'wenjun'
+  showToast = () => {
+    Indicator.toast('提交成功')
   }
-  render () {
+
+  showLoading = () => {
+    const lid = Indicator.showLoading()
+    setTimeout(() => {
+      Indicator.hideLoading(lid)
+    }, 3000)
+  }
+
+  render() {
     return (
-      <div>
-        IndicatorDemo
-        <div className={style.test}>{this.state.name}</div>
-      </div>
+      <DemoPage title='Indicator'>
+        <SlotColumnView hAlign='center' slot={30}>
+          <Button
+            onClick={this.showLoading}
+            color='#99CC33'
+            width={600}
+            height={80}
+          >
+            loading
+          </Button>
+          <Button
+            onClick={this.showToast}
+            color='#99CCFF'
+            width={600}
+            height={80}
+          >
+            toast
+          </Button>
+        </SlotColumnView>
+      </DemoPage>
     )
   }
 }
